478ec403f49b52a36eca632250d74b44086d9f99
fari <fari@appswiz.com>
2014-09-08 13:30:41 +1000

    Convert informatel html pages to MVC

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,9 +18,29 @@ namespace Informatel.Website.Controllers
         }
 
         [HttpPost]
-        public ActionResult Submit()
+        public ActionResult Index(string first_name, string last_name, string email, string phone, string message)
         {
-            return View();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    MailMessage mail = new MailMessage();
+                    mail.To.Add("noreply@appswiz.com");
+                    mail.From = new MailAddress(email);
+                    mail.Subject = first_name + ' ' + last_name;
+                    mail.Body = phone +' '+ message;
+
+                    System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("infemail01");
+                    smtp.Send(mail);
+
+                    return RedirectToAction("Index", "Contact");
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
         }
 	}
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,6 +14,33 @@ namespace Informatel.Website.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(string first_name, string last_name, string email, string phone)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    MailMessage mail = new MailMessage();
+                    mail.To.Add("noreply@appswiz.com");
+                    mail.From = new MailAddress(email);
+                    mail.Subject = first_name + ' ' + last_name;
+                    mail.Body = phone;
+
+                    System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("infemail01");
+                    smtp.Send(mail);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
         }
 	}
 }
